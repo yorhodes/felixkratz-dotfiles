02dd74ebf3280db4312ff368d37246884cd4938d
Yorke Rhodes <email@yorke.dev>
2023-01-05 15:03:22 -0500

    custom changes

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,25 +1,34 @@
 #!/usr/bin/env sh
 
 # Color Palette
-export BLACK=0xff181926
-export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
-export TRANSPARENT=0x00000000
+BLACK=0xff181926
+WHITE=0xffcad3f5
+RED=0xffed8796
+GREEN=0xffa6da95
+BLUE=0xff8aadf4
+YELLOW=0xffeed49f
+ORANGE=0xfff5a97f
+MAGENTA=0xffc6a0f6
+GREY=0xff939ab7
+TRANSPARENT=0x00000000
+
+DARK_MODE=$(osascript -e 'tell application "System Events" to tell appearance preferences to get dark mode')
 
 # General bar colors
-export BAR_COLOR=0xcc24273a
-export ICON_COLOR=$WHITE # Color of all icons
-export LABEL_COLOR=$WHITE # Color of all labels
-export BACKGROUND_1=0xff3c3e4f
-export BACKGROUND_2=0xff494d64
+if [ "$DARK_MODE" = "true" ]; then
+    BAR_COLOR=0xcc24273a
+    ICON_COLOR=$WHITE # Color of all icons
+    LABEL_COLOR=$WHITE # Color of all labels
+else
+    BAR_COLOR=0xcce1e3e4
+    ICON_COLOR=$BLACK # Color of all icons
+    LABEL_COLOR=$BLACK # Color of all labels
+fi
 
-export POPUP_BACKGROUND_COLOR=$BLACK
-export POPUP_BORDER_COLOR=$WHITE
+POPUP_BACKGROUND_COLOR=$BAR_COLOR
+POPUP_BORDER_COLOR=$GREEN
 
-export SHADOW_COLOR=$BLACK
+SHADOW_COLOR=$BLACK
+
+# Item specific special colors
+SPOTIFY_GREEN=0xff1db954
